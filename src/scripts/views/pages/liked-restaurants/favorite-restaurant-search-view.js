f8f65690685851a21fdc9edf3cbe1f934a318df3
mocheru <herunoor09@gmail.com>
2023-06-03 16:11:06 +0700

    update the search-bar

--- src/scripts/views/pages/liked-restaurants/favorite-restaurant-search-view.js
+++ src/scripts/views/pages/liked-restaurants/favorite-restaurant-search-view.js
@@ -1,38 +1,38 @@
-import { createRestaurantItemTemplate } from '../../templates/template-creator';
+import { createRestaurantItemTemplate } from '../../templates/template-creator'
 
 class FavoriteRestaurantSearchView {
-    getTemplate() {
-        return `
+  getTemplate () {
+    return `
           <div class="content">
-            <input id="query" type="text">
+            <input id="query" type="text" placeholder="find your favorite" class="search_tab">
             <h2 class="content__heading">Your Liked Restaurant</h2>
               <div id="restaurants" class="restaurants">
               </div>
         </div>
-        `;
-    }
+        `
+  }
 
-    runWhenUserIsSearching(callback) {
-        document.getElementById('query').addEventListener('change', (event) => {
-          callback(event.target.value);
-        });
-    }
+  runWhenUserIsSearching (callback) {
+    document.getElementById('query').addEventListener('change', (event) => {
+      callback(event.target.value)
+    })
+  }
 
-    showFavoriteRestaurants(restaurants = []) {
-      let html;
-      if (restaurants.length) {
-        html = restaurants.reduce((carry, restaurant) => carry.concat(createRestaurantItemTemplate(restaurant)), '');
-      } else {
-        html = this._getEmptyRestaurantTemplate();
-      }
-      document.getElementById('restaurants').innerHTML = html;
-
-      document.getElementById('restaurants').dispatchEvent(new Event('restaurants:updated'));
+  showFavoriteRestaurants (restaurants = []) {
+    let html
+    if (restaurants.length) {
+      html = restaurants.reduce((carry, restaurant) => carry.concat(createRestaurantItemTemplate(restaurant)), '')
+    } else {
+      html = this._getEmptyRestaurantTemplate()
     }
+    document.getElementById('restaurants').innerHTML = html
 
-    _getEmptyRestaurantTemplate() {
-      return '<div class="restaurant-item__not__found">Tidak ada restaurant untuk ditampilkan</div>';
-    }
+    document.getElementById('restaurants').dispatchEvent(new Event('restaurants:updated'))
+  }
+
+  _getEmptyRestaurantTemplate () {
+    return '<div class="restaurant-item__not__found">Tidak ada restaurant untuk ditampilkan</div>'
+  }
 }
 
-export default FavoriteRestaurantSearchView;
+export default FavoriteRestaurantSearchView
